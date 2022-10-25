ab00dd006f046bab576bed1381ce12dca1a1c46f
Maksim <you@epic.twod@mail.ru>
2022-10-25 22:31:14 +0300

    commit 1.x; not done

--- dynamicarray/dynamicarray.cpp
+++ dynamicarray/dynamicarray.cpp
@@ -1,20 +1,56 @@
-﻿// dynamicarray.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
 
-#include <iostream>
+template <typename _t>
+
+class darry
+{
+public:
+
+    darry(int msize)
+    {
+        _size = msize;
+        for (int i = 0; i < size_of(darr); i++)
+        {
+            darr[i] = {};
+        }
+    }
+
+    int size_of(_t elem)
+    {
+        return sizeof(elem) / sizeof(_t);
+    }
+
+    size_t getSize()
+    {
+        return _size;
+    }
+
+    _t getArray()
+    {
+        return darr;
+    }
+
+    _t getElem(int index)
+    {
+        return darr[index];
+    }
+
+    void setSize(int size)
+    {
+        _size = size;
+    }
+
+private:
+    size_t _size;
+    _t* darr = new _t[_size];
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int abc = 5;
+    darry<int> object(5);
+    for (int i = 0; i < object.getSize(); i++)
+    {
+        std::cout << object.getElem(i) << std::endl;
+    }
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
